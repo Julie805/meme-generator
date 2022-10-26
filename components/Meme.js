020c12ba8ad0c68f0d9dee937ada4b385b38799a
Julie M <seejulielee@gmail.com>
2022-10-25 20:59:32 -0700

    Cleaned up comments

--- components/Meme.js
+++ components/Meme.js
@@ -3,11 +3,6 @@ import memesData from "../memesData.js"
 
 export default function Meme() {
     const [memeImage, setMemeImage] = React.useState("")
-    /**
-     * Challenge: Save the random meme URL in state
-     * - Below the div.form, add an <img /> and set the
-     *   src to the new `memeImage` state you created
-     */
     
     function getMemeImage() {
         const memesArray = memesData.data.memes
